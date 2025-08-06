5dfebad9736cbc23979bddee8e2f5d9a3ca53e2b
Shelly Fourer <shellyfourer@gmail.com>
2025-08-07 01:38:32 +0300

    Reorganizing
    Split portfolio sections into unique pages. Added additional HTML and CSS files. Created separate header HTML and CSS to enable overlapping navigation via a container loaded with JS

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-document.querySelector('.scroll-down').addEventListener('click', function() {
-    const aboutSection = document.getElementById('about');
-    if (aboutSection) {
-        aboutSection.scrollIntoView({ behavior: 'smooth' });
-    }
-});
 
 
