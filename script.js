6065eaf084475c4ada14fdba5a111e9d6f3b5ac6
Shelly Fourer <shellyfourer@gmail.com>
2025-08-23 01:42:26 +0300

    Responsive design 1.0
    I created the first version of the header and landing page being responsive to various monitors by optimising relative units instead of a large amount of media queries

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-
-
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+        const navbar = document.querySelector('.navbar');
+        const main = document.querySelector('main');
+        if (navbar && main) {
+            main.style.paddingTop = navbar.offsetHeight + 'px';
+        }
+    });
